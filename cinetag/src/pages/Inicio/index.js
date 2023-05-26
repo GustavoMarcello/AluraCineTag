77be74578995e4a2feff72308d76728cd905c59a
Gustavo Goetze Marcello <ggm@intranet.pitang.com>
2023-05-26 14:05:48 -0300

    finish first step course

---
+++ cinetag/src/pages/Inicio/index.js
@@ -0,0 +1,15 @@
+import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape"
+
+function Inicio(){
+    return (
+        <>
+            // auto fechamento em tags que não enviam nada
+            <Cabecalho />
+            <Rodape />
+        </>
+    )
+        
+}
+
+export default Inicio;
