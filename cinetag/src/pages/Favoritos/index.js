7439dd58c89244c80f0f11cfd561e7202c91e7ee
Gustavo Goetze Marcello <ggm@intranet.pitang.com>
2023-05-29 16:43:39 -0300

    Create PaginaBase, Player, NaoEncontrada pages

--- cinetag/src/pages/Favoritos/index.js
+++ cinetag/src/pages/Favoritos/index.js
@@ -2,8 +2,10 @@ import styles from './Favoritos.module.css'
 import Banner from "components/Banner";
 import Card from 'components/Card';
 import Titulo from "components/Titulo";
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 function Favoritos() {
+    const { favorito } = useFavoritoContext();
     return (
         <>
             <Banner imagem="favoritos" />
@@ -11,7 +13,9 @@ function Favoritos() {
                 <h1>Meus Favoritos</h1>
             </Titulo>
             <section className={styles.container}>
-                <Card id='1' titulo='picachu' capa='https://i.pinimg.com/originals/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.png' />
+                { favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
             </section>
         </>
     )
